3c4570ce1ff5c906446c4f6bdf07c4224408a7d4
llamicron <llamicron@gmail.com>
2021-02-19 16:55:04 -0600

    Adds basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,215 @@
 #![allow(non_snake_case)]
 
 const HELP_PAGE: &'static str = include_str!("help_page");
-use std::io::{Write, stdout, stdin};
-use termion::input::TermRead;
-use termion::event::Key;
-use termion::raw::IntoRawMode;
-use termion::clear;
 
-// macro_rules! prompt {
-//     ($type:ty) => {
-//         match prompt().parse::<$type>() {
-//             Ok(value) => Some(value),
-//             Err(e) => {
-//                 eprintln!("Couldn't get value of the correct type, try again. ({})", e);
-//                 None
-//             }
-//         }
-//     };
-// }
+use std::{
+    io::{Write, stdout, stdin},
+    process::exit,
+    println,
+    unimplemented
+};
 
-
-// fn prompt() -> String {
-//     print!(">  ");
-//     stdout().flush().unwrap();
-
-//     let mut buffer = String::new();
-//     match stdin().read_line(&mut buffer) {
-//         Ok(_) => {},
-//         Err(e) => {
-//             eprintln!("Error: couldn't read line: {}", e);
-//         }
-//     }
-
-//     String::from(buffer.trim())
-// }
-
-fn main() {
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    let stdin = stdin();
-    let mut cmd_buffer = String::new();
-    let mut history: Vec<String> = vec![];
-
-    println!("{}", clear::All);
-
-
-    write!(stdout, "> ");
-    for c in stdin.keys() {
-        // Clear the current line.
-        // write!(stdout, "{}{}", termion::cursor::Goto(1, 1), termion::clear::CurrentLine).unwrap();
-        // Print the key we type...
-        match c.unwrap() {
-            // Exit.
-            Key::Ctrl('q') => break,
-            Key::Char(c)   => {
-                cmd_buffer.push(c);
-                write!(stdout, "{}", c);
-            },
-
-            Key::Backspace => {
-                cmd_buffer.pop();
-                write!(stdout, "^H ^H");
-            },
-            Key::Left      => println!("<left>"),
-            Key::Right     => println!("<right>"),
-            Key::Up        => println!("<up>"),
-            Key::Down      => println!("<down>"),
-            _              => {},
+macro_rules! prompt {
+    ($type:ty) => {
+        match prompt().parse::<$type>() {
+            Ok(value) => Some(value),
+            Err(e) => {
+                eprintln!("Couldn't get value of the correct type, try again. ({})", e);
+                None
+            }
         }
-        // writeln!(stdout, "> {}", cmd_buffer);
+    };
+}
 
-        // Flush again.
-        stdout.flush().unwrap();
+
+fn prompt() -> String {
+    print!(">  ");
+    stdout().flush().unwrap();
+
+    let mut buffer = String::new();
+    match stdin().read_line(&mut buffer) {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("Error: couldn't read line: {}", e);
+        }
     }
 
-    // loop {
-    //     if let Some(cmd) = prompt!(String) {
-    //         match cmd.as_str() {
-    //             _ => println!("{}", HELP_PAGE)
-    //         }
-    //     }
-    // }
+    buffer.trim().to_owned()
+}
+
+#[derive(Debug)]
+struct CN7500Config {
+    port: String,
+    addr: u16,
+    baudrate: u32
+}
+
+#[derive(Debug)]
+struct STR1Config {
+    port: String,
+    addr: u8,
+    baudrate: u32
+}
+
+fn main() {
+    println!("Brewdrivers {}", env!("CARGO_PKG_VERSION"));
+
+    let mut str1_config = STR1Config {
+        port: String::from("/dev/ttyUSB0"),
+        addr: 0x01,
+        baudrate: 9600
+    };
+
+    let mut cn7500_config = CN7500Config {
+        port: String::from("/dev/ttyUSB0"),
+        addr: 0x16,
+        baudrate: 9600
+    };
+
+    loop {
+
+        let input = match prompt!(String) {
+            Some(value) => value,
+            _ => continue,
+        };
+
+        let args = input.split(' ').collect::<Vec<&str>>();
+
+        // No arg commands
+        if args.len() == 1 {
+            // Basic commands, no args
+            match args[0] {
+                "help" => println!("{}", HELP_PAGE),
+                "quit" => exit(0),
+                "config" => {
+                    println!("{:#?}", str1_config);
+                    println!("{:#?}", cn7500_config);
+                }
+                _ => {}
+            }
+            continue;
+        }
+
+
+        // Regular command groups
+        // STR1 Config group
+        match args[0].to_lowercase().as_str() {
+            "str1.port" => {
+                str1_config.port = String::from(args[1]);
+                println!("{:#?}", str1_config);
+                continue;
+            },
+            "str1.baudrate" => {
+                if let Ok(baud) = args[1].parse::<u32>() {
+                    str1_config.baudrate = baud;
+                    println!("{:#?}", str1_config);
+                }
+                continue;
+            },
+            "str1.addr" => {
+                if let Ok(addr) = args[1].parse::<u8>() {
+                    str1_config.addr = addr;
+                    println!("{:#?}", str1_config);
+                }
+                continue;
+            }
+            _ => {}
+        }
+
+        // CN7500 Config group
+        match args[0].to_lowercase().as_str() {
+            "cn7500.port" => {
+                cn7500_config.port = String::from(args[1]);
+                println!("{:#?}", cn7500_config);
+                continue;
+            },
+            "cn7500.baudrate" => {
+                if let Ok(baud) = args[1].parse::<u32>() {
+                    cn7500_config.baudrate = baud;
+                    println!("{:#?}", cn7500_config);
+                }
+                continue;
+            },
+            "cn7500.addr" => {
+                if let Ok(addr) = args[1].parse::<u16>() {
+                    cn7500_config.addr = addr;
+                    println!("{:#?}", cn7500_config);
+                }
+                continue;
+            }
+            _ => {}
+        }
+
+        // STR1 actions
+        match args[0] {
+            "str1.relay" => {
+                if args.len() == 3 {
+                    // Set relay
+                    println!("Relay {} set to {}", args[1], "On");
+                } else if args.len() == 2 {
+                    // Get relay
+                    println!("Relay {} is {}", args[1], "On");
+                }
+                continue;
+            },
+            "str1.set_cn" => {
+                if let Ok(new_cn) = args[1].parse::<u8>() {
+                    // Set controller number
+                    println!("controller number set to {}", new_cn);
+                } else {
+                    println!("Invalid controller number, should be 0-255");
+                }
+                continue;
+            }
+            _ => {}
+        }
+
+        // CN7500 actions
+        match args[0] {
+            "cn7500.pv" => {
+                // Get PV
+                println!("{}", "118.3");
+                continue;
+            },
+            "cn7500.sv" => {
+                if args.len() == 2 {
+                    // Set SV
+                    println!("SV set to {}", args[1]);
+                } else {
+                    // Get SV
+                    println!("SV: {}", "150.0");
+                }
+                continue;
+            },
+            "cn7500.run" => {
+                // Run the cn7500
+                println!("CN7500 is running...");
+                continue;
+            },
+            "cn7500.stop" => {
+                // Stop the cn7500
+                println!("CN7500 stopped.");
+                continue;
+            },
+            "cn7500.set_units" => {
+                if args.len() < 2 {
+                    println!("Not enough args. Should be F or C");
+                    continue;
+                }
+
+                match args[1].to_uppercase().as_str() {
+                    "F" => {
+                        // Set to F
+                        println!("CN7500 set to Fahrenheit");
+                    },
+                    "C" => {
+                        // Set to C
+                        println!("CN7500 set to Celsius");
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    }
 }
